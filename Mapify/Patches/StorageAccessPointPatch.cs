fb54e4d5310ea378fd9428f9a4a2f80437edc4b8
Pierce Thompson <insprill@gmail.com>
2023-04-03 06:00:55 -0400

    Fix StorageAccessPointLostAndFound not loading correctly
    It references the StorageBase in the vanilla scene, so when we move it to our scene it loses that reference. This is a little hacky for now, perhaps in the future we could add a script to all objects that reference a StorageBase and assign them the correct one once the scene has been loaded.

---
+++ Mapify/Patches/StorageAccessPointPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Mapify.Patches
+{
+    [HarmonyPatch(typeof(StorageAccessPoint), "OnEnable")]
+    public static class StorageAccessPoint_OnEnable_Patch
+    {
+        public static bool Prefix(StorageAccessPoint __instance)
+        {
+            if (__instance is StorageAccessPointLostAndFound)
+                __instance.storage = GameObject.Find("StorageLostAndFound").GetComponent<StorageBase>();
+            return true;
+        }
+    }
+}

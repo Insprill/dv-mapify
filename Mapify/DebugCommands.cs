bda06428cb16c4bf0f3c04e8550e43e3ae2a40d7
Pierce Thompson <insprill@gmail.com>
2023-04-03 02:14:24 -0400

    Simplify command registration, add station info command

--- Mapify/DebugCommands.cs
+++ Mapify/DebugCommands.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Reflection;
 using CommandTerminal;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Mapify
 {
@@ -10,8 +12,16 @@ namespace Mapify
         {
             try
             {
-                Terminal.Shell.AddCommand("mapify.licenses", GrantAllLicenses, help: "Grants all licenses");
-                Terminal.Autocomplete.Register("mapify.licenses");
+                MethodInfo[] methods = typeof(DebugCommands).GetMethods(BindingFlags.Static | BindingFlags.NonPublic);
+                foreach (MethodInfo method in methods)
+                {
+                    if (!(Attribute.GetCustomAttribute(method, typeof(RegisterCommandAttribute)) is RegisterCommandAttribute commandAttribute))
+                        return;
+                    Action<CommandArg[]> proc = (Action<CommandArg[]>)Delegate.CreateDelegate(typeof(Action<CommandArg[]>), method);
+                    Terminal.Shell.AddCommand(commandAttribute.Name, proc, commandAttribute.MinArgCount, commandAttribute.MaxArgCount, commandAttribute.Help, commandAttribute.Hint, commandAttribute.Secret);
+                    Terminal.Autocomplete.Register(commandAttribute.Name);
+                    Main.Logger.Log($"Registered command {commandAttribute.Name}");
+                }
             }
             catch (Exception e)
             {
@@ -19,6 +29,7 @@ namespace Mapify
             }
         }
 
+        [RegisterCommand("mapify.licenses", Help = "Grants all licenses", MaxArgCount = 0)]
         private static void GrantAllLicenses(CommandArg[] args)
         {
             foreach (GeneralLicenseType generalLicense in Enum.GetValues(typeof(GeneralLicenseType)))
@@ -26,6 +37,21 @@ namespace Mapify
             foreach (JobLicenses jobLicense in Enum.GetValues(typeof(JobLicenses)))
                 LicenseManager.AcquireJobLicense(jobLicense);
             Debug.Log("Granted all licenses");
+        }
+
+        [RegisterCommand("mapify.stationInfo", Help = "Prints information about all stations", MaxArgCount = 0)]
+        private static void PrintStationInfo(CommandArg[] args)
+        {
+            foreach (StationController station in Object.FindObjectsOfType<StationController>())
+            {
+                StationInfo stationInfo = station.stationInfo;
+                string str = "\n";
+                str += $"{stationInfo.Name}\n";
+                str += $"+-- Type: '{stationInfo.Type}'\n";
+                str += $"+-- Yard ID: '{stationInfo.YardID}'\n";
+                str += $"+-- Color: '{ColorUtility.ToHtmlStringRGBA(stationInfo.StationColor)}'";
+                Debug.Log(str);
+            }
         }
     }
 }

92322454fb6ac9e5108310c8d1315fcbe0be2b79
Pierce Thompson <insprill@gmail.com>
2023-04-03 05:09:07 -0400

    Fix NRE when a PlayerDistanceGameObjectsDisabler's optimizingGameObjects list is null

---
+++ Mapify/Patches/PlayerDistanceGameObjectsDisablerPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace Mapify.Patches
+{
+    [HarmonyPatch(typeof(PlayerDistanceGameObjectsDisabler), "OnDisable")]
+    public static class PlayerDistanceGameObjectsDisablerPatch
+    {
+        // Fixes an NRE
+        public static bool Prefix(PlayerDistanceGameObjectsDisabler __instance)
+        {
+            if (__instance.optimizingGameObjects != null)
+                return true;
+            __instance.StopAllCoroutines();
+            return false;
+        }
+    }
+}

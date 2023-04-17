a58092ab322d6b5dcd8f0ff607074998e4f739d0
Pierce Thompson <insprill@gmail.com>
2023-04-16 23:14:13 -0400

    Cleanup package importing/exporting

---
+++ MapifyEditor/Package/PackageCreator.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mapify.Editor
+{
+    public static class PackageCreator
+    {
+        private const string UNITY_PACKAGE_EXT_NAME = "unitypackage";
+        private const string EXPORT_ASSET_PATH = "Assets/Mapify";
+        private const string LAST_EXPORTED_KEY = "Mapify.Package.LastExportPath";
+        private const string LAST_IMPORTED_KEY = "Mapify.Package.LastImportPath";
+
+        [MenuItem("Mapify/Package/Export")]
+        public static void Export()
+        {
+            string path = EditorPrefs.GetString(LAST_EXPORTED_KEY);
+            if (string.IsNullOrWhiteSpace(path) || Directory.GetParent(path)?.Exists == false)
+            {
+                path = EditorUtility.SaveFilePanel("Package", "../", $"mapify.{UNITY_PACKAGE_EXT_NAME}", UNITY_PACKAGE_EXT_NAME);
+                if (string.IsNullOrWhiteSpace(path)) return;
+                EditorPrefs.SetString(LAST_EXPORTED_KEY, path);
+            }
+
+            AssetDatabase.ExportPackage(EXPORT_ASSET_PATH, path, ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
+            Debug.Log($"Package exported to '{path}'!");
+        }
+
+        [MenuItem("Mapify/Package/Import")]
+        public static void Import()
+        {
+            string path = EditorPrefs.GetString(LAST_IMPORTED_KEY);
+            if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+            {
+                path = EditorUtility.OpenFilePanel("Package", "../", $"{UNITY_PACKAGE_EXT_NAME}");
+                if (string.IsNullOrWhiteSpace(path)) return;
+                EditorPrefs.SetString(LAST_IMPORTED_KEY, path);
+            }
+
+            AssetDatabase.ImportPackage(path, false);
+            Debug.Log($"Package imported from '{path}'!");
+        }
+
+        [MenuItem("Mapify/Package/Remove Saved Paths")]
+        public static void RemoveSavedPath()
+        {
+            EditorPrefs.DeleteKey(LAST_EXPORTED_KEY);
+            EditorPrefs.DeleteKey(LAST_IMPORTED_KEY);
+            Debug.Log("Removed saved paths.");
+        }
+    }
+}

eefd6bc899b0f90474e33e38695223e6b69f78c5
Pierce Thompson <insprill@gmail.com>
2023-04-16 23:30:02 -0400

    Add validation for VR settings

--- MapifyEditor/Export/Validators/ProjectValidator.cs
+++ MapifyEditor/Export/Validators/ProjectValidator.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Mapify.Editor.Utils;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -34,6 +36,19 @@ namespace Mapify.Editor.Validators
                 yield return Result.Error($"The spawn position's X and Z values must be within the world's bounds (0-{mapInfo.worldSize}", mapInfo);
             if (spawnPos.y < mapInfo.waterLevel)
                 yield return Result.Error($"The spawn position must be above the water level ({mapInfo.waterLevel}", mapInfo);
+
+            #endregion
+
+            #region VR
+
+            // Obsolete VR settings.
+#pragma warning disable 0618
+            if (!PlayerSettings.virtualRealitySupported) yield return Result.Error("VR support isn't enabled");
+            if (!PlayerSettings.singlePassStereoRendering) yield return Result.Error("VR Stereo Rendering Mode isn't set to Single Pass");
+            string[] sdks = PlayerSettings.GetVirtualRealitySDKs(BuildTargetGroup.Standalone);
+            if (!sdks.Contains("Oculus")) yield return Result.Error("Oculus support isn't enabled");
+            if (!sdks.Contains("OpenVR")) yield return Result.Error("OpenVR support isn't enabled");
+#pragma warning restore 0618
 
             #endregion
         }

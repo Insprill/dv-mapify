54e1f1f5e96f23fd5d3d5d149be976610ad8c1d0
Pierce Thompson <insprill@gmail.com>
2023-04-22 13:40:24 -0400

    Bump to 0.1.0 🥳

--- openfolders.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Code
-xdg-open TestMap/Assets/Mapify/Scripts &
-xdg-open "$HOME"/.local/share/Steam/steamapps/common/Derail\ Valley/Mods/Mapify &
-xdg-open "$HOME"/.local/share/Steam/steamapps/compatdata/588030/pfx/drive_c/users/steamuser/AppData/LocalLow/Altfuture/Derail\ Valley &

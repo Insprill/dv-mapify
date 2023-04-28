56132d71aac5dcc6852da50a01e361f6f50c0c03
Pierce Thompson <insprill@gmail.com>
2023-04-27 22:05:28 -0400

    Update RailwayBuilder to use the RailwayMeshUpdater

--- Mapify/Components/RailwayBuilder.cs
+++ Mapify/Components/RailwayBuilder.cs
@@ -1,11 +1,7 @@
-using System.Collections.Generic;
 using System.Reflection;
-using DV;
-using DV.PointSet;
 using HarmonyLib;
 using Mapify.Editor.Utils;
 using Mapify.Utils;
-using Unity.Jobs;
 using UnityEngine;
 using UnityModManagerNet;
 
@@ -16,22 +12,14 @@ namespace Mapify.Components
         private const float MAX_DIST = 100.0f;
         private const float CONNECT_THRESHOLD = 4f;
 
-        private static readonly FieldInfo RailwayMeshGenerator_Field_spatialHash = AccessTools.DeclaredField(typeof(RailwayMeshGenerator), "spatialHash");
-        private static readonly FieldInfo RailwayMeshGenerator_Field_activeChunks = AccessTools.DeclaredField(typeof(RailwayMeshGenerator), "activeChunks");
-        private static readonly FieldInfo RailwayMeshGenerator_Field_activeJobs = AccessTools.DeclaredField(typeof(RailwayMeshGenerator), "activeJobs");
-        private static readonly FieldInfo RailwayMeshGenerator_Field_prevCellId = AccessTools.DeclaredField(typeof(RailwayMeshGenerator), "prevCellId");
-        private static readonly FieldInfo TrackChunkSpatialHash_Field_doneAdding = AccessTools.DeclaredField(typeof(TrackChunkSpatialHash), "doneAdding");
-        private static readonly FieldInfo TrackChunkSpatialHash_Field_lookupCells = AccessTools.DeclaredField(typeof(TrackChunkSpatialHash), "lookupCells");
         private static readonly FieldInfo RailTrack_Field_pointSet = AccessTools.DeclaredField(typeof(RailTrack), "pointSet");
 
-        private RailwayMeshGenerator railwayMeshGenerator;
         private KeyBinding keyBinding;
         private bool isPlacing;
         private RailTrack beingPlaced;
 
         private void Awake()
         {
-            railwayMeshGenerator = FindObjectOfType<RailwayMeshGenerator>();
             keyBinding = new KeyBinding();
             keyBinding.Change(KeyCode.T, false, true, false);
         }
@@ -94,7 +82,7 @@ namespace Mapify.Components
             beingPlaced.ConnectOutToClosestBranch();
             if (beingPlaced.generateColliders)
                 beingPlaced.CreateCollider();
-            AddOrUpdateTrack(railwayMeshGenerator, beingPlaced);
+            RailwayMeshUpdater.UpdateTrack(beingPlaced);
             beingPlaced = null;
             isPlacing = false;
         }
@@ -113,49 +101,7 @@ namespace Mapify.Components
             RailTrack.pointSets.Remove(beingPlaced);
             RailTrack.pointSets.Add(beingPlaced, beingPlaced.GetPointSet());
             RailTrack_Field_pointSet.SetValue(beingPlaced, null);
-            AddOrUpdateTrack(railwayMeshGenerator, beingPlaced);
-        }
-
-        // todo: there's a leak somewhere that causes performance issues
-        private static void AddOrUpdateTrack(RailwayMeshGenerator railwayMeshGenerator, RailTrack track)
-        {
-            TrackChunkSpatialHash spatialHash = (TrackChunkSpatialHash)RailwayMeshGenerator_Field_spatialHash.GetValue(railwayMeshGenerator);
-            TrackChunkSpatialHash_Field_doneAdding.SetValue(spatialHash, false);
-
-            Dictionary<Vector2Int, List<TrackChunk>> lookupCells = (Dictionary<Vector2Int, List<TrackChunk>>)TrackChunkSpatialHash_Field_lookupCells.GetValue(spatialHash);
-            foreach (KeyValuePair<Vector2Int, List<TrackChunk>> kvp in lookupCells)
-                for (int i = kvp.Value.Count - 1; i >= 0; i--)
-                    if (kvp.Value[i].track == track)
-                        kvp.Value.RemoveAt(i);
-
-            EquiPointSet railPointSet = track.GetPointSet();
-            foreach (EquiPointSet.Point point in railPointSet.points)
-            {
-                TrackChunk trackChunk = spatialHash.Add(railPointSet, point);
-                trackChunk.isSleepers = false;
-                trackChunk.track = track;
-            }
-
-            EquiPointSet sleepersPointSet = EquiPointSet.ResampleEquidistant(railPointSet, track.baseType.sleeperDistance, track.baseType.sleeperDistance * 0.5f, true);
-            foreach (EquiPointSet.Point point in sleepersPointSet.points)
-            {
-                TrackChunk trackChunk = spatialHash.Add(sleepersPointSet, point);
-                trackChunk.isSleepers = true;
-                trackChunk.track = track;
-            }
-
-            spatialHash.DoneAdding();
-            RailwayMeshGenerator_Field_prevCellId.SetValue(railwayMeshGenerator, new Vector2Int(int.MinValue, int.MaxValue));
-            Dictionary<Vector2Int, List<TrackChunk>> activeChunks = (Dictionary<Vector2Int, List<TrackChunk>>)RailwayMeshGenerator_Field_activeChunks.GetValue(railwayMeshGenerator);
-            foreach (KeyValuePair<Vector2Int, List<TrackChunk>> kvp in activeChunks)
-            foreach (TrackChunk chunk in kvp.Value)
-                if (chunk.track == track)
-                    chunk.ReleasePoolObjects();
-            activeChunks.Clear();
-
-            List<(MeshSweeperJob meshSweeperJob, JobHandle jobHandle, Mesh mesh)> activeJobs =
-                (List<(MeshSweeperJob meshSweeperJob, JobHandle jobHandle, Mesh mesh)>)RailwayMeshGenerator_Field_activeJobs.GetValue(railwayMeshGenerator);
-            activeJobs.Clear();
+            RailwayMeshUpdater.UpdateTrack(beingPlaced);
         }
     }
 }

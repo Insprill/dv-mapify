648794959656743447ad118472fce4ef6588bbad
Pierce Thompson <insprill@gmail.com>
2023-05-21 01:39:37 -0400

    Add a warning and validation if the Unity version is incorrect
    Closes #17.

---
+++ MapifyEditor/UnityVersionChecker.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Mapify.Editor
+{
+    [InitializeOnLoad]
+    public class UnityVersionChecker
+    {
+        private const string REQUIRED_UNITY_VERSION = "2019.4.22f1";
+        private const string INSTALLATION_DOCS_URL = "https://dv-mapify.readthedocs.io/en/latest/creatingmaps/project-setup/#installing-unity";
+        public static readonly string ERROR_MESSAGE =
+            $"Incorrect Unity version {Application.unityVersion}! Please use {REQUIRED_UNITY_VERSION}. For more information, please check out the documentation.";
+
+        static UnityVersionChecker()
+        {
+            if (IsCorrectVersion())
+                return;
+            if (EditorUtility.DisplayDialog("Mapify", ERROR_MESSAGE, "Open Documentation", "Ok"))
+                Application.OpenURL(INSTALLATION_DOCS_URL);
+        }
+
+        public static bool IsCorrectVersion()
+        {
+            return Application.unityVersion == REQUIRED_UNITY_VERSION;
+        }
+    }
+}

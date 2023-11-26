56dde12257f0971091c4cfbac030747a805a26b7
t0stiman <18124323+t0stiman@users.noreply.github.com>
2023-11-26 16:54:06 +0100

    duplicated switches patch

--- Mapify/SceneInitializers/Railway/RailwaySceneInitializer.cs
+++ Mapify/SceneInitializers/Railway/RailwaySceneInitializer.cs
@@ -1,5 +1,7 @@
 using System.Linq;
+using Mapify.Editor;
 using Mapify.Utils;
+using Mapify.Editor.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,6 +14,14 @@ namespace Mapify.SceneInitializers.Railway
 
         public override void Run()
         {
+            //switches get duplicated if we don't do this. This is a buǵ but I can't figure out the cause of the buǵ.
+            foreach (var switchComponent in scene.GetAllComponents<Switch>())
+            {
+                var switchObject = switchComponent.gameObject;
+                Mapify.LogWarning($"Deleting switch '{switchObject.name}' to avoid duplication");
+                GameObject.Destroy(switchObject);
+            }
+
             Transform railwayParent = WorldMover.Instance.NewChild("[railway]").transform;
             foreach (Transform transform in scene.GetRootGameObjects().Select(go => go.transform))
                 transform.SetParent(railwayParent);

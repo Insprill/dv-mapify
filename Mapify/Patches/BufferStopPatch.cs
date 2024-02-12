6d374952792775561c865efbc5815f32718417a6
Pierce Thompson <52843537+Insprill@users.noreply.github.com>
2024-02-12 14:17:17 -0500

    Use Krafs.Publicizer instead of reflection and hard-coded patch names (#65)

--- Mapify/Patches/BufferStopPatch.cs
+++ Mapify/Patches/BufferStopPatch.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using Mapify.Map;
@@ -11,11 +10,9 @@ namespace Mapify.Patches
     /// <summary>
     ///     Allows us to set the break velocity, and mass after break for Buffer Stops.
     /// </summary>
-    [HarmonyPatch(typeof(BufferStop), "OnTriggerEnter")]
+    [HarmonyPatch(typeof(BufferStop), nameof(BufferStop.OnTriggerEnter))]
     public static class BufferStop_OnTriggerEnter_Patch
     {
-        private static readonly FieldInfo BufferStop_Field_SQR_BREAK_BUFFER_VELOCITY_THRESHOLD = AccessTools.DeclaredField(typeof(BufferStop), "SQR_BREAK_BUFFER_VELOCITY_THRESHOLD");
-
         /// <summary>
         ///     Replaces the logic removed in the Transpiler.
         /// </summary>
@@ -24,7 +21,7 @@ namespace Mapify.Patches
         {
             Rigidbody attachedRigidbody = other.attachedRigidbody;
             float breakSpeed = Maps.IsDefaultMap
-                ? (float)BufferStop_Field_SQR_BREAK_BUFFER_VELOCITY_THRESHOLD.GetValue(null)
+                ? BufferStop.SQR_BREAK_BUFFER_VELOCITY_THRESHOLD
                 : __instance.GetComponent<Editor.BufferStop>().breakSpeed * 3.6f;
             return attachedRigidbody != null && attachedRigidbody.velocity.sqrMagnitude <= breakSpeed * breakSpeed;
         }
@@ -48,11 +45,10 @@ namespace Mapify.Patches
         /// <seealso cref="Prefix" />
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            float threshold = (float)BufferStop_Field_SQR_BREAK_BUFFER_VELOCITY_THRESHOLD.GetValue(null);
             List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
 
             for (int i = 0; i < codes.Count; i++)
-                if (codes[i].opcode == OpCodes.Ldc_R4 && ((float)codes[i].operand).Eq(threshold))
+                if (codes[i].opcode == OpCodes.Ldc_R4 && ((float)codes[i].operand).Eq(BufferStop.SQR_BREAK_BUFFER_VELOCITY_THRESHOLD))
                 {
                     for (int j = i - 5; j <= i + 1; j++)
                         codes[j].opcode = OpCodes.Nop;
--- Mapify/Patches/StationProceduralJobGeneratorPatch.cs
+++ Mapify/Patches/StationProceduralJobGeneratorPatch.cs
@@ -2,7 +2,7 @@ using HarmonyLib;
 
 namespace Mapify.Patches
 {
-    [HarmonyPatch(typeof(StationProceduralJobGenerator), "GenerateBaseCargoTrainData")]
+    [HarmonyPatch(typeof(StationProceduralJobGenerator), nameof(StationProceduralJobGenerator.GenerateBaseCargoTrainData))]
     public static class StationProceduralJobGenerator_GenerateBaseCargoTrainData_Patch
     {
         private static void Prefix(ref int minNumberOfCars, int maxNumberOfCars)

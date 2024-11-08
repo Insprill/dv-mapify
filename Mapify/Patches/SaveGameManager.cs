4911d8195ba0c05c51bf9feca864e73cc3eee963
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-11-08 16:21:26 +0100

    [BuildMode] also save on autosave

--- Mapify/Patches/SaveGameManager.cs
+++ Mapify/Patches/SaveGameManager.cs
@@ -1,11 +1,7 @@
 ﻿using System.IO;
 using DV.Common;
-using DV.UI;
-using DV.UserManagement;
-using DV.Utils;
 using HarmonyLib;
 using Mapify.BuildMode;
-using Mapify.Editor;
 using Mapify.Map;
 
 namespace Mapify.Patches
@@ -13,12 +9,12 @@ namespace Mapify.Patches
     /// <summary>
     /// save the game -> save placed assets
     /// </summary>
-    [HarmonyPatch (typeof(UserProfileProvider), nameof(UserProfileProvider.SaveGame))]
-    public static class UserProfileProviderPatch_SaveGame_Patch
+    [HarmonyPatch (typeof(SaveGameManager), nameof(SaveGameManager.Save))]
+    public static class SaveGameManager_Save_Patch
     {
         private static void Postfix(ISaveGame __result)
         {
-            //TODO separate save button?
+            // result is null if saving is not allowed at this point
             if(__result == null){ return; }
 
             string xmlPath;

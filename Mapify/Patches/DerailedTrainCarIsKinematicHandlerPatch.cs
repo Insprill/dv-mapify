77323f07b3bdfd095d3981fb97b7f0b7a7b77cac
t0stiman <18124323+t0stiman@users.noreply.github.com>
2025-08-20 16:22:56 +0200

    fix derailment (hack)

---
+++ Mapify/Patches/DerailedTrainCarIsKinematicHandlerPatch.cs
@@ -0,0 +1,15 @@
+﻿using DV;
+using HarmonyLib;
+
+namespace Mapify.Patches
+{
+    [HarmonyPatch(typeof(DerailedTrainCarIsKinematicHandler), nameof(DerailedTrainCarIsKinematicHandler.UpdateIsKinematicDependingOnLoadedCells))]
+    public static class DerailedTrainCarIsKinematicHandler_UpdateIsKinematicDependingOnLoadedCells_Patch
+    {
+        private static bool Prefix(DerailedTrainCarIsKinematicHandler __instance)
+        {
+            __instance.train.rb.isKinematic = false;
+            return false;
+        }
+    }
+}
